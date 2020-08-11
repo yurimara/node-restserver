4dc74de63980eb6d15c14f83f21f34d2e0a99eda
Yurimara <yurimara@gmail.com>
2020-08-11 17:52:33 -0400

    variable de entorno mongo heroku

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://strider:TBHE3MvAAaRIJtaO@cluster0.ldpo7.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
